28ab85a1362df231d28e7f133574e1e414fb451e
ppeace1216 <riay2021@gmail.com>
2022-09-02 17:40:45 +0900

    java casting

--- ch02/src/sec1/Var03.java
+++ ch02/src/sec1/Var03.java
@@ -1,21 +1,20 @@
 package sec1;
 
 public class Var03 {
-
 	public static void main(String[] args) {
-		boolean a=true;	//true 또는 false만 입력가능
+		//primitive type(기본 자료형) : 하나의 데이터만 저장가능한 구조
+		boolean a=true;
 		//정수
-		byte b=20, c=127;	//-128~127까지만 저장 가능 ->1byte(8bit = 2의 8승 = 256가지) 초과 오버플로우,미만 언더플로우
-		short d=6450;	//2byte(16bit = 2의 16승 = 65536가지)
+		byte b=20, c=127;  //-128~127 -> 1byte(8bit = 2의 8승 = 256가지)
+		short d=6450;   //2byte(16bit)
 		int e=65000;	//4byte(32bit)
-		long f=540000;	//8byte(64bit)
+		long f=650000;	//8byte(64bit)
 		//실수
 		float g=3.14f;	//4byte(32bit)
-		double h=3.14;	//8byte(64bit) float처럼 f를 뒤에 꼭 안붙여도 됨
+		double h=3.14;	//8byte(64bit)
 		//문자/문자열
-		char i='k';		//2byte(16bit)
+		char i='k';		//2byte(16bit) 
 		
-		String J="kim";	//
+		String j="kim"; 
 	}
-
 }
