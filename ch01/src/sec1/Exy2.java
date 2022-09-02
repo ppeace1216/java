2362c261dbb918aaa9444439548c7e02a85c6040
ppeace1216 <riay2021@gmail.com>
2022-09-02 09:57:01 +0900

    java starting and webtest

---
+++ ch01/src/sec1/Exy2.java
@@ -0,0 +1,9 @@
+package sec1;
+
+//기능 클래스 모델링
+public class Exy2 {
+	public void method1(){ //method1:메서드명
+		System.out.println("하이~예제2!"); //실행문장 끝에 ;(세미콜론=컨티뉴) 
+		
+	}
+}
---
+++ ch02/src/sec1/Var02.java
@@ -0,0 +1,11 @@
+package sec1;
+
+public class Var02 {	//scope : 인지되는 범위나 시기
+	int a=10,  b=20;	//글로벌 (전역) 변수 - 현재 클래스 내부에서 어디서든지 인식
+	
+	public static void main(String[] args) {
+		int a=100, b=200;	//로컬(지역)변수 - 현재 매서드 안에서만 인식
+		System.out
+	}
+//매개 변수
+}
